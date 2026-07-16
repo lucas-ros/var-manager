ebaa0b18380a55e0b8defc15d7489c476e009cc8
lucas <auslc15@gmail.com>
2026-07-15 23:44:25 -0300

    chore: adjust test_vm.c to run first vm_init tests

--- tests/test_vm.c
+++ tests/test_vm.c
@@ -1,5 +1,6 @@
 #include "unity.h"
 #include "vm.h"
+#include "var_map.h"
 
 void setUp(void) {}
 void tearDown(void) {}
@@ -8,8 +9,26 @@ void test_compilation(void) {
     TEST_ASSERT_EQUAL(1, 1);
 }
 
+void test_vm_init_valid(void) {
+    int result = vm_init(var_list, var_list_size);
+    TEST_ASSERT_EQUAL(0, result);
+}
+
+void test_vm_init_var_list_null(void) {
+    int result = vm_init(NULL, var_list_size);
+    TEST_ASSERT_EQUAL(-1, result);
+}
+
+void test_vm_init_var_size_zero(void) {
+    int result = vm_init(NULL, var_list_size);
+    TEST_ASSERT_EQUAL(-1, result);
+}
+
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_compilation);
+    RUN_TEST(test_vm_init_valid);
+    RUN_TEST(test_vm_init_var_list_null);
+    RUN_TEST(test_vm_init_var_size_zero);
+
     return UNITY_END();
 }
