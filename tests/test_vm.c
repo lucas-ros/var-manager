899976b30218cc22c7a04bf83183bf9dd065321d
lucas <auslc15@gmail.com>
2026-07-16 00:13:58 -0300

    chore: create tests for vm_init() and vm_deinit()

--- tests/test_vm.c
+++ tests/test_vm.c
@@ -3,14 +3,20 @@
 #include "var_map.h"
 
 void setUp(void) {}
-void tearDown(void) {}
-
-void test_compilation(void) {
-    TEST_ASSERT_EQUAL(1, 1);
+void tearDown(void) {
+    vm_deinit();
 }
 
 void test_vm_init_valid(void) {
     int result = vm_init(var_list, var_list_size);
+    TEST_ASSERT_EQUAL(0, result);
+}
+
+void test_vm_deinit_valid(void) {
+    if(vm_init(var_list, var_list_size) < 0) {
+        TEST_FAIL();
+    }
+    int result = vm_deinit();
     TEST_ASSERT_EQUAL(0, result);
 }
 
@@ -20,15 +26,29 @@ void test_vm_init_var_list_null(void) {
 }
 
 void test_vm_init_var_size_zero(void) {
-    int result = vm_init(NULL, var_list_size);
+    int result = vm_init(var_list, 0);
+    TEST_ASSERT_EQUAL(-1, result);
+}
+
+void test_vm_deinit_invalid(void) {
+    int result = vm_deinit();
+    TEST_ASSERT_EQUAL(-1, result);
+}
+
+void test_vm_init_already_initialized_var_list(void) {
+    vm_init(var_list, var_list_size);
+    int result = vm_init(var_list, var_list_size);
     TEST_ASSERT_EQUAL(-1, result);
 }
 
 int main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_vm_init_valid);
+    RUN_TEST(test_vm_deinit_valid);
     RUN_TEST(test_vm_init_var_list_null);
     RUN_TEST(test_vm_init_var_size_zero);
+    RUN_TEST(test_vm_deinit_invalid);
+    RUN_TEST(test_vm_init_already_initialized_var_list);
 
     return UNITY_END();
 }
